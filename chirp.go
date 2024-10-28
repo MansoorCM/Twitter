659aee4e1010ddb5658a4f6f12f02cc5e6ae2c48
MansoorCM <manzoorcm96@gmail.com>
2024-10-28 21:41:36 +0530

    Remove profane words from Chirp

--- chirp.go
+++ chirp.go
@@ -3,18 +3,19 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 type Chirp struct {
 	Body string `json:"body"`
 }
 
-type errorResponse struct {
-	Error string `json:"error"`
+type CleanChirp struct {
+	CleanedBody string `json:"cleaned_body"`
 }
 
-type returnVals struct {
-	Valid bool `json:"valid"`
+type errorResponse struct {
+	Error string `json:"error"`
 }
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +35,26 @@ func validateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	resp := returnVals{Valid: true}
+	chirpAfterCleaning := replaceProfaneWords(chirp.Body)
+	resp := CleanChirp{CleanedBody: chirpAfterCleaning}
 	respondWithJson(w, resp, http.StatusOK)
+}
+
+func replaceProfaneWords(s string) string {
+	profaneWords := getProfaneWords()
+
+	words := strings.Split(s, " ")
+
+	for i, word := range words {
+		lowercaseWord := strings.ToLower(word)
+		if _, ok := profaneWords[lowercaseWord]; ok {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func getProfaneWords() map[string]struct{} {
+	profaneWords := map[string]struct{}{"kerfuffle": {}, "sharbert": {}, "fornax": {}}
+	return profaneWords
 }
